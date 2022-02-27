fc1c234af7282156d43e4e2a0b843487024548f8
DRuban <radies@rambler.ru>
2022-02-27 21:55:48 +0300

    Убран вывод в консоль

--- src/main/java/HomeWork3.java
+++ src/main/java/HomeWork3.java
@@ -3,10 +3,8 @@ import java.util.Random;
 
 public class HomeWork3 {
     public static void main(String[] args) {
-        System.out.println("1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, " +
-                "1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;");
+//    1.
         int [] arr ={1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
-        System.out.println("Массив до    " + Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 1){
                 arr[i] = 0;
@@ -15,51 +13,35 @@ public class HomeWork3 {
                 arr[i] = 1;
             }
         }
-        System.out.println("Массив после " + Arrays.toString(arr));
-        System.out.println();
-        System.out.println("2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100");
 
+//     2.
         int [] arr100 = new int[100];
         for (int i = 0; i < arr100.length;) {
             arr100[i] = ++i;
         }
-        System.out.println("Результат: " + Arrays.toString(arr100));
-        System.out.println();
-        System.out.println("3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2");
+
+//      3.
         int [] arr3 ={1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        System.out.println("Массив до    " + Arrays.toString(arr3));
         for (int i = 0; i < arr3.length; i++) {
             if (arr3[i] < 6){
                 arr3[i] = arr3[i]*2;
             }
         }
-        System.out.println("Массив после " + Arrays.toString(arr3));
 
-        System.out.println("4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью " +
-        "цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно). " +
-        "Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны, " +
-        "то есть [0][0], [1][1], [2][2], …, [n][n]");
+//     4.
         int [] [] arr4 = new int [9][9];
-        System.out.println("Результат: ");
-
         for (int i = 0; i < arr4.length; i++) {
             for (int j = 0; j < arr4[i].length; j++) {
                 if(i==j || i == arr4.length - j - 1 ){
                    arr4[i][j] = 1;
                 }
-                System.out.print(arr4[i][j] + " ");
             }
-            System.out.println();
         }
-        System.out.println();
 
-        System.out.println("5. Написать метод, принимающий на вход два аргумента: len и initialValue, и возвращающий одномерный массив " +
-        "типа int длиной len, каждая ячейка которого равна initialValue");
+//     5.
         int [] arr5 = initArr(3,10);
-        System.out.println("Результат: " + Arrays.toString(arr5));
-        System.out.println();
 
-        System.out.println("6. * Задать одномерный массив и найти в нем минимальный и максимальный элементы");
+//     6.
         Random random = new Random();
         int [] arr6 =  new int[10];
         int maxArr6Elem =0,minArr6Elem =0, maxValue =0, minValue = 0;
@@ -80,32 +62,21 @@ public class HomeWork3 {
                 }
             }
         }
-        System.out.println("Массив " + Arrays.toString(arr6));
-        System.out.println("MaxElem № " + maxArr6Elem + " MaxValue " + maxValue  );
-        System.out.println("MinElem № " + minArr6Elem + " MinValue " + minValue  );
-        System.out.println();
+//        System.out.println("Массив " + Arrays.toString(arr6));
+//        System.out.println("MaxElem № " + maxArr6Elem + " MaxValue " + maxValue  );
+//        System.out.println("MinElem № " + minArr6Elem + " MinValue " + minValue  );
 
-        System.out.println("7. ** Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть " +
-           "true, если в массиве есть место, в котором сумма левой и правой части массива равны.");
+//     7.
         int [] arr7 = {2, 1,1,1,1};
-        System.out.println("Массив " + Arrays.toString(arr7));
-        System.out.println("Результат: " + checkSum(arr7));
-        System.out.println();
+        checkSum(arr7);
 
-        System.out.println("8. *** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, " +
-        "или отрицательным), при этом метод должен сместить все элементы массива на n позиций. " +
-        "Элементы смещаются циклично. Для усложнения задачи нельзя пользоваться вспомогательными массивами. " +
-        "Примеры: [ 1, 2, 3 ] при n = 1 (на один вправо) -> [ 3, 1, 2 ]; [ 3, 5, 6, 1] при n = -2 (на два влево) -> " +
-        "[ 6, 1, 3, 5 ]. При каком n в какую сторону сдвиг можете выбирать сами.");
+//     8.
         int [] arr8 = {1,2,3,4,5};
         shiftArr(2,arr8);
-        System.out.println("Результат: " + Arrays.toString(arr8));
     }
-//        5. Написать метод, принимающий на вход два аргумента: len и initialValue, и возвращающий одномерный массив
-//        типа int длиной len, каждая ячейка которого равна initialValue;
 
+//  5.
     private static int [] initArr    (int len,int initialValue){
-        System.out.println("Элементов " + len + " Значения " + initialValue);
         int [] arr;
         if (len < 0){
             len = 0;
@@ -116,13 +87,8 @@ public class HomeWork3 {
         }
         return arr;
     }
-//        7. ** Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть
-//        true, если в массиве есть место, в котором сумма левой и правой части массива равны.
-//**Примеры:
-//        checkBalance([2, 2, 2, 1, 2, 2, ||| 10, 1]) → true, т.е. 2 + 2 + 2 + 1 + 2 + 2 = 10 + 1
-//        checkBalance([1, 1, 1, ||| 2, 1]) → true, т.е. 1 + 1 + 1 = 2 + 1
-//
-//        граница показана символами |||, эти символы в массив не входят и не имеют никакого отношения к ИЛИ.
+
+//   7.
      private static boolean checkSum (int [] arr){
          for (int i = 0; i < arr.length; i++) {
              int sumLeft = 0, sumRight = 0;
@@ -142,17 +108,11 @@ public class HomeWork3 {
          return false;
      }
 
-//        8. *** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным,
-//        или отрицательным), при этом метод должен сместить все элементы массива на n позиций.
-//        Элементы смещаются циклично. Для усложнения задачи нельзя пользоваться вспомогательными массивами.
-//        Примеры: [ 1, 2, 3 ] при n = 1 (на один вправо) -> [ 3, 1, 2 ]; [ 3, 5, 6, 1] при n = -2 (на два влево) ->
-//        [ 6, 1, 3, 5 ]. При каком n в какую сторону сдвиг можете выбирать сами.
+//   8.
      private static int [] shiftArr (int n, int[] arr){
-         System.out.println("Сдвиг " + n);
-         System.out.println("Массив " + Arrays.toString(arr));
 //       Сократим количество сдвигов
          n = n % arr.length;
-//       Чтобы не изменять направление сдвига в циклк инвертируем сдвиг
+//       Чтобы не изменять направление сдвига в цикле инвертируем сдвиг
          if (n < 0) {
              n = arr.length + n;
          }
@@ -166,7 +126,6 @@ public class HomeWork3 {
                  arr[j] = arr[j + 1];
                  arr[j+ 1] = elem;
              }
-             System.out.println(i + Arrays.toString(arr));
          }
          return arr;
      }
