ad69aaba38f565f8d33d06d92c545fa48decee3e
DRuban <radies@rambler.ru>
2022-03-06 00:10:22 +0300

    home_work_5

---
+++ src/main/java/Employee.java
@@ -0,0 +1,34 @@
+public class Employee {
+    private String fio,position,email,tel;
+    private float salary;
+    private int age;
+
+    public Employee (String fio
+                    ,String position
+                    ,String email
+                    ,String tel
+                    ,float salary
+                    ,int age){
+        this.fio      = fio;
+        this.position = position;
+        this.email    = email;
+        this.tel      = tel;
+        this.salary   = salary;
+        this.age      = age;
+    }
+    public void print(){
+        System.out.println("ФИО       : " + fio);
+        System.out.println("Должность : " + position);
+        System.out.println("Email     : " + email);
+        System.out.println("Телефон   : " + tel);
+        System.out.println("Зарплата  : " + salary);
+        System.out.println("Возраст   : " + age);
+        System.out.println();
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
+
