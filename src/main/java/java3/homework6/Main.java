8c532f72cfa5649d2eb7c03acdfcec986ebbb7e0
DRuban <radies@rambler.ru>
2022-09-07 08:23:39 +0300

    java3_homework7 Создать класс, который может выполнять «тесты». В качестве тестов выступают классы с наборами методов, снабженных аннотациями @Test. Класс, запускающий тесты, должен иметь статический метод start(Class testClass), которому в качестве аргумента передается объект типа Class. Из «класса-теста» вначале должен быть запущен метод с аннотацией @BeforeSuite, если он присутствует. Далее запускаются методы с аннотациями @Test, а по завершении всех тестов – метод с аннотацией @AfterSuite. К каждому тесту необходимо добавить приоритеты (int-числа от 1 до 10), в соответствии с которыми будет выбираться порядок их выполнения. Если приоритет одинаковый, то порядок не имеет значения. Методы с аннотациями @BeforeSuite и @AfterSuite должны присутствовать в единственном экземпляре. Если это не так – необходимо бросить RuntimeException при запуске «тестирования». P.S. Это практическое задание – проект, который пишется «с нуля». Данная задача не связана напрямую с темой тестирования через JUnit

---
+++ src/main/java/java3/homework6/Main.java
@@ -0,0 +1,60 @@
+package java3.homework6;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, IllegalAccessException, NoSuchMethodException, InstantiationException {
+        Class testClass = Class.forName("java3.homework6.Test1");
+        start(testClass);
+        start(Class.forName("java3.homework6.Test2"));
+    }
+    public static void start(Class testClass) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, InstantiationException {
+        Method [] methods = testClass.getDeclaredMethods();
+        int beforeSuite = 0;
+        int afterSuite = 0;
+        Method before = null;
+        Method after = null;
+        String mesError;
+        boolean badPriority = false;
+        for (Method method:methods){
+            if(method.getAnnotation(BeforeSuite.class)!=null){
+                beforeSuite++;
+                before= method;
+            }
+            if(method.getAnnotation(AfterSuite.class)!=null){
+                afterSuite++;
+                after=method;
+            }
+            Test test = method.getAnnotation(Test.class);
+            if (test != null &&
+                    (test.priority() > 10 || test.priority() < 1) ){
+               badPriority =true;
+            }
+        }
+        mesError = (beforeSuite > 1 ? "Методов BeforeSuite должно быть не больше 1 " : "")
+                 + (afterSuite  > 1 ? "Методов AfterSuite должно быть не больше 1 "  : "")
+                 + (badPriority     ? "Приоритет Test должен быть от 1 до 10 "       : "");
+
+        if(!mesError.equals( "")){
+            throw new RuntimeException(mesError);
+        }
+        Object testObject = testClass.getDeclaredConstructor().newInstance();
+        if(before!=null){
+            before.invoke(testObject);
+        }
+        for (int i = 1; i <= 10 ; i++) {
+            for (Method method:methods){
+                Test test = method.getAnnotation(Test.class);
+                if (test != null &&
+                        test.priority() == i){
+                    method.invoke(testObject);
+
+                }
+            }
+        }
+        if(after!= null){
+            after.invoke(testObject);
+        }
+    }
+}
---
+++ src/main/java/java3/homework6/Test2.java
@@ -0,0 +1,26 @@
+package java3.homework6;
+
+public class Test2 {
+    @AfterSuite
+    public void after1(){
+        System.out.println("AfterSuite");
+    }
+    @AfterSuite
+    public void after2(){
+        System.out.println("AfterSuite");
+    }
+
+    @BeforeSuite
+    public void before1(){
+        System.out.println("BeforeSuite");
+    }
+    @BeforeSuite
+    public void before2(){
+        System.out.println("BeforeSuite");
+    }
+
+    @Test(priority = 15)
+    public void test(){
+        System.out.println("priority 15");
+    }
+}
